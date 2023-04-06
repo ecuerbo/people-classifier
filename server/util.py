6ca4df332b6ff1457d2e26f9c8a32be0fa0150be
Elbon Cuerbo <elbon.cuerbo@gmail.com>
2023-04-06 19:04:26 +0800

    expanding my ds capabilities, hoping to finish this project before vacation ends

---
+++ server/util.py
@@ -0,0 +1,9 @@
+def classify_image(image_base64_data, file_path=None):
+    pass
+
+def get_b64_test_image_for_virat():
+    with open("b64.txt") as f:
+        return f.read()
+
+if __name__ == '__main__':
+    print(classify_image(get_b64_test_image_for_virat(), ;None))
